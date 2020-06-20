42815f8100eea63252b48d78943d3eb6b98adfa1
chablisdog <colefig7@gmail.com>
2020-06-20 18:31:16 -0400

    commit project 2 assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,29 @@
-## Put comments here that give an overall description of what your
-## functions do
+# In this case, the makeCacheMatrix function is the parent function, assuming the matrix is invertible 
 
-## Write a short comment describing this function
+makeCacheMatrix <- function(x = matrix()) { 
+        inv <- NULL 
+        set <- function(y){ #setting the value of the matrix
+                x <<- y # managing the environement levels, with << we are able to modify variables in the parent levels 
+                inv <<- NULL
+        } # set function is the child function 
+        get <- function() {x}
+        setInverse <- function(inverse) {inv <<- inverse}
+        getInverse <- function() {inv} 
+        list(set = set, get = get, 
+             setInverse = setInverse, 
+             getInverse = getInverse)
+} 
 
-makeCacheMatrix <- function(x = matrix()) {
-
-}
-
-
-## Write a short comment describing this function
+# cacheSolve computes the inverse of the created function from above
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inv <- x$getInverse() #first need to check if the inverse has already been calculated
+        if(!is.null(inv)) {
+                message("pulling cache data")
+                return(inv)
+        }
+        mat <- x$get()
+        inv <- solve(mat, ...)
+        x$setInverse(inv)
+        inv
 }
